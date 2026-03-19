543524834559a0f454d09fefd52653feefa7083a
Mani Bhati <manibhati@Manis-MacBook-Air.local>
2026-03-19 11:05:57 +0530

    fixed fetch + route crash

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,7 @@ app.use(express.static(path.join(__dirname, "frontend")));
    DEFAULT ROUTE (LOGIN PAGE)
 ================================ */
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend", "login.html"));
 });
 
